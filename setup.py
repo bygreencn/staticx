c6716e6596e4ebb08282646e64ac5729dbf25266
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2020-01-11 01:02:28 -0500

    Remove support for Python 2.7

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Derived from https://github.com/JonathonReinhart/scuba
-from __future__ import print_function
 from setuptools import setup, Command, find_packages
 from distutils.command.build import build
 import os
@@ -41,7 +40,7 @@ setup(
     name = 'staticx',
     version = get_dynamic_version(),
     description = 'Build static self-extracting app from dynamic executable',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.5',
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -49,10 +48,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
@@ -87,7 +83,6 @@ setup(
     },
     install_requires = [
         'pyelftools',
-        'backports.lzma;python_version<"3.3"',
     ],
 
     # http://stackoverflow.com/questions/17806485
